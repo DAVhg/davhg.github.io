b75dcd35f71f81eaa69b439ed2ec70d6eefe4f75
David <david98herrera@gmail.com>
2020-08-07 17:07:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,71 @@ var i = 0,
 // Typerwrite text content. Use a pipe to indicate the start of the second line "|".  
 var textArray = [
     "",
-    "CANCEL CANCEL CULTURE. F*CK INFLUENCER CULTURE. DEPLATFORM THEM. REWRITE THE GORITHMS.",
+    "var i = 0",
+    "a = 0, ",
+    "isBackspacing = false, ",
+    "isParagraph = false; ",
+
+    "var speedForward = 20, ",
+    "speedWait = 800", 
+    "speedBetweenLines = 400, ",
+    "typeWriter(textArray); ",
+
+    "function typeWriter(id, ar) {",
+    "var element = $( + id),",
+    "aString = ar[a], ",
+    "eHeader = element.children(h1), ",
+    "eParagraph = element.children(h1); ",
+
+   
+    "if (!isBackspacing) {",
+
+        
+    "if (i < aString.length) {",
+
+    "if (aString.charAt(i) == " | ") {",
+    "isParagraph = true;",
+                "eHeader.removeClass(cursor);",
+               " eParagraph.addClass(cursor);",
+               " i++;",
+               " setTimeout(function () { typeWriter(id, ar); }, speedBetweenLines);",
+           " } else {",
+                "if (!isParagraph) {",
+                   " eHeader.text(eHeader.text() + aString.charAt(i));",
+               " } else {",
+                  "  eParagraph.text(eParagraph.text() + aString.charAt(i));",
+                "}",
+               " i++;",
+               " setTimeout(function () { typeWriter(id, ar); }, speedForward);",
+           " }",
+
+       " } else if (i == aString.length) {",
+
+           " isBackspacing = true;",
+           " setTimeout(function () { typeWriter(id, ar); }, speedWait);",
+
+       " }",
+
+    "} else {",
+
+    "if (eHeader.text().length > 0 || eParagraph.text().length > 0) {",
+
+            "if (eParagraph.text().length > 0) {",
+            "    eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));",
+            " } else if (eHeader.text().length > 0) {",
+            "     eParagraph.removeClass(cursor);",
+            "     eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));",
+            "  }",
+                "setTimeout(function () { typeWriter(id, ar); }, speedBackspace); ",
+
+        "} else { ",
+
+            "isBackspacing = false; ",
+           " i = 0; ",
+           " isParagraph = false; ",
+           " a = (a + 1) % ar.length; ",
+          "  setTimeout(function () { typeWriter(id, ar); }, 50); ",
+
     "",
 ];
 
