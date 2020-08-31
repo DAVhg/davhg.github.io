1c83976038b96b4c36db5d415bcb7f9d11910952
David <david98herrera@gmail.com>
2020-09-01 01:35:14 +0200

    home

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ var i = 0,
 
 // Typerwrite text content. Use a pipe to indicate the start of the second line "|".  
 var textArray = [
-    "A problem has been detected and Windows has been shut down to prevent damage to your computer. | If this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps: | Check to make sure any new hardware or software is properly installed.I this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need. | If problems continue, disable or remove any newly installed hardware or software.Disable BIOS memory options such as caching or shadowing.If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode. | Technical information: | *** STOP: 0x000000FE(0x00000008, 0x000000006, 0x00000009, 0x847075cc)",
+    "",
+    "A problem has been detected and Windows has been shut down to prevent damage to your computer. | If this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps: | Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need. | If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode. | Technical information: | *** STOP: 0x000000FE(0x00000008, 0x000000006, 0x00000009, 0x847075cc)",
 ];
 
 // Speed (in milliseconds) of typing.
-var speedForward = 20, //Typing Speed
+var speedForward = 10, //Typing Speed
     speedWait = 400, // Wait between typing and backspacing
     speedBetweenLines = 400, //Wait between first and second lines
     speedBackspace = 10; //Backspace Speed
