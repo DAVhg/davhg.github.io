1711a434f986abc37297475072f6758cc6a35f1b
David <david98herrera@gmail.com>
2020-09-01 13:57:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ var i = 0,
 var textArray = [
     "",
     //"A problem has been detected and Windows has been shut down to prevent damage to your computer. | If this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps: | Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need. | If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode. | Technical information: | *** STOP: 0x000000FE(0x00000008, 0x000000006, 0x00000009, 0x847075cc)",
-    "welcome. | thanks for checking in. | this web is for personal use and educational purposes only. | i will not be saving any personal information of you. | enjoy it. | you will now be redirected to a home page with multiple options and projects that i have been working on for some time. | The web will now start loading.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|  ",
+    "Welcome. | Thanks for checking in. | This web is for personal use and educational purposes only. | I will not be saving any personal information of you as is not required for the website to perform properly. | You will now be redirected to a home page with multiple options and projects that i have been working on for some time. | The web will now start loading.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|  ",
 ];
 
 // Speed (in milliseconds) of typing.
-var speedForward = 10, //Typing Speed
-    speedWait = 400, // Wait between typing and backspacing
+var speedForward = 20, //Typing Speed
+    speedWait = 600, // Wait between typing and backspacing
     speedBetweenLines = 400, //Wait between first and second lines
     speedBackspace = 10; //Backspace Speed
 
@@ -55,8 +55,8 @@ function typeWriter(id, ar) {
             // If full string has been typed, switch to backspace mode.
         } else if (i == aString.length) {
 
-            isBackspacing = true;
-            setTimeout(function () { typeWriter(id, ar); }, speedWait);
+             isBackspacing = true;
+             setTimeout(function () { typeWriter(id, ar); }, speedWait);
 
         }
 
