556f00c4cdb86e8d8fd66b1c5ffa82c18f87026f
David Herrera <39059200+DAVhg@users.noreply.github.com>
2020-09-11 09:21:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var i = 0,
 var textArray = [
     "",
     //"A problem has been detected and Windows has been shut down to prevent damage to your computer. | If this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps: | Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need. | If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode. | Technical information: | *** STOP: 0x000000FE(0x00000008, 0x000000006, 0x00000009, 0x847075cc)",
-    //"Welcome. | Thanks for checking in. | This web is for personal use and educational purposes only. | I will not be saving any personal information of you as is not required for the website to perform properly. | You will now be redirected to a home page with multiple options and projects that i have been working on for some time. | The web will now start loading.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|  ",
-    "$$22444336688833664443666 2 6444 93322 22332233, 4777222244427777 7666777 888444777744482777633!! 833 26666 6.255522337778666, 8866 2233777729999666, 3288833 <3"
+    "Welcome. | Thanks for checking in. | This web is for personal use and educational purposes only. | I will not be saving any personal information of you as is not required for the website to perform properly. | You will now be redirected to a home page with multiple options and projects that i have been working on for some time. | The web will now start loading.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|.|  ",
+    //"$$22444336688833664443666 2 6444 93322 22332233, 4777222244427777 7666777 888444777744482777633!! 833 26666 6.255522337778666, 8866 2233777729999666, 3288833 <3"
 ];
 
 // Speed (in milliseconds) of typing.
